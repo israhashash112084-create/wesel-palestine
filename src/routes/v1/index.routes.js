2ae4e8970efca79eb332bf815bf0ad574f739e9a
Aseel_Dwikat <aseeldwaikat62@gmail.com>
2026-03-14 23:01:43 +0200

    feat(alerts): connect alerts routes to main router

--- src/routes/v1/index.routes.js
+++ src/routes/v1/index.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import authRoute from '#modules/auth/auth.routes.js';
 import reportsRoute from '#modules/reports/reports.routes.js';
+import alertsRoute from '#modules/alerts/alerts.routes.js';
 
 const router = Router();
 router.use('/auth', authRoute);
 router.use('/reports', reportsRoute);
+router.use('/alerts', alertsRoute);
 
 export default router;
