1be74278b3696cae0df6a15fab622bf675fc8634
IsraHashash <s12216931@stu.najah.edu>
2026-03-19 03:54:52 +0200

    refactor(incidents): rename verified fields to moderated in schema and migration

---
+++ prisma/migrations/20260319004408_rename_verified_to_moderat/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `verified_at` on the `incidents` table. All the data in the column will be lost.
+  - You are about to drop the column `verified_by` on the `incidents` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "incidents" DROP CONSTRAINT "incidents_verified_by_fkey";
+
+-- DropIndex
+DROP INDEX "idx_incidents_verified_by";
+
+-- AlterTable
+ALTER TABLE "incidents" DROP COLUMN "verified_at",
+DROP COLUMN "verified_by",
+ADD COLUMN     "moderade_by" UUID,
+ADD COLUMN     "moderaded_at" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "reports" ADD COLUMN     "incident_id" INTEGER;
+
+-- CreateIndex
+CREATE INDEX "idx_incidents_verified_by" ON "incidents"("moderade_by");
+
+-- CreateIndex
+CREATE INDEX "reports_incident_id_idx" ON "reports"("incident_id");
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_incident_id_fkey" FOREIGN KEY ("incident_id") REFERENCES "incidents"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "incidents" ADD CONSTRAINT "incidents_moderade_by_fkey" FOREIGN KEY ("moderade_by") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
